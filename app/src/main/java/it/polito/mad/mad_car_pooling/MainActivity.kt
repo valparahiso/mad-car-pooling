8b5c27a6d50a8c122898e2788793dfc284c1a955
Enrico <bravienrico@yahoo.it>
2021-03-25 09:00:21 +0100

    initial commit

---
+++ app/src/main/java/it/polito/mad/mad_car_pooling/MainActivity.kt
@@ -0,0 +1,11 @@
+package it.polito.mad.mad_car_pooling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

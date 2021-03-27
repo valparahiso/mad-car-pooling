12aa4164a5a645d7172807b5cde5af63e08eb092
Enrico <bravienrico@yahoo.it>
2021-03-27 11:56:40 +0100

    fixed menu bar

--- app/src/main/java/it/polito/mad/mad_car_pooling/MainActivity.kt
+++ app/src/main/java/it/polito/mad/mad_car_pooling/MainActivity.kt
@@ -1,11 +1,17 @@
 package it.polito.mad.mad_car_pooling
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_profile)
+        //setContentView(R.layout.activity_show_profile)
+
+        val intent = Intent(this, ShowProfileActivity::class.java)
+        startActivity(intent)
     }
+
 }
--- app/src/main/java/it/polito/mad/mad_car_pooling/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/mad_car_pooling/ShowProfileActivity.kt
@@ -2,10 +2,18 @@ package it.polito.mad.mad_car_pooling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.file_menu, menu)
+        return true
     }
 }

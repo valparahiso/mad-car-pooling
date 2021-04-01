5ceb43c13bf59a219f11fe2916a17eb5bee6122d
Enrico <bravienrico@yahoo.it>
2021-04-01 11:35:21 +0200

    implemented setEditText() in EditProfileActivity.kt

--- app/src/main/java/it/polito/mad/mad_car_pooling/EditProfileActivity.kt
+++ app/src/main/java/it/polito/mad/mad_car_pooling/EditProfileActivity.kt
@@ -1,13 +1,16 @@
 package it.polito.mad.mad_car_pooling
 
+import android.app.backup.FullBackupDataOutput
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.ContextMenu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
+import android.widget.EditText
 import android.widget.ImageButton
 
 class EditProfileActivity : AppCompatActivity() {
@@ -16,8 +19,10 @@ class EditProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit_profile)
 
         val imageButton = findViewById<ImageButton>(R.id.camera)
-
         registerForContextMenu(imageButton)
+
+        setEditText()
+
     }
 
     override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
@@ -39,5 +44,21 @@ class EditProfileActivity : AppCompatActivity() {
             }
             else -> super.onContextItemSelected(item)
         }
+    }
+
+    private fun setEditText() {
+        val fullName: String? = intent.getStringExtra("group02.lab1.FULL_NAME")
+        val nickName: String? = intent.getStringExtra("group02.lab1.NICK_NAME")
+        val location: String? = intent.getStringExtra("group02.lab1.LOCATION")
+        val email: String? = intent.getStringExtra("group02.lab1.EMAIL")
+
+        val fullNameET = findViewById<EditText>(R.id.edit_fullName)
+        fullNameET.setText(fullName)
+        val nicknameET = findViewById<EditText>(R.id.edit_nickName)
+        nicknameET.setText(nickName)
+        val locationET = findViewById<EditText>(R.id.edit_location)
+        locationET.setText(location)
+        val emailET = findViewById<EditText>(R.id.edit_email)
+        emailET.setText(email)
     }
 }
--- app/src/main/java/it/polito/mad/mad_car_pooling/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/mad_car_pooling/ShowProfileActivity.kt
@@ -6,12 +6,21 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 
 class ShowProfileActivity : AppCompatActivity() {
+    private var fullName: String = "Mario Rossi"
+    private var nickName: String = "mariorossi"
+    private var location: String = "ovunque"
+    private var emain: String = "mario.rossi@polito.it"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
-
+        findViewById<TextView>(R.id.fullName).text =  fullName
+        findViewById<TextView>(R.id.nickname).text =  nickName
+        findViewById<TextView>(R.id.email).text =  emain
+        findViewById<TextView>(R.id.location).text =  location
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,8 +31,16 @@ class ShowProfileActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         //Log.d("POLITOMAD","onOptionsItemSelected()")
-        val intent = Intent(this, EditProfileActivity::class.java)
-        startActivity(intent)
+        editProfile()
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun editProfile() {
+        val intent = Intent(this, EditProfileActivity::class.java)
+        intent.putExtra("group02.lab1.FULL_NAME", fullName)
+        intent.putExtra("group02.lab1.NICK_NAME", nickName)
+        intent.putExtra("group02.lab1.EMAIL", emain)
+        intent.putExtra("group02.lab1.LOCATION", location)
+        startActivityForResult(intent, 1)
     }
 }

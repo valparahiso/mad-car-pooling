a4458faf80f02e2631a8b3b444f746bfaeaa17df
Enrico <bravienrico@yahoo.it>
2021-04-01 10:28:54 +0200

    added floating context menu

--- app/src/main/java/it/polito/mad/mad_car_pooling/EditProfileActivity.kt
+++ app/src/main/java/it/polito/mad/mad_car_pooling/EditProfileActivity.kt
@@ -2,10 +2,24 @@ package it.polito.mad.mad_car_pooling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuInflater
+import android.view.View
+import android.widget.ImageButton
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        val imageButton = findViewById<ImageButton>(R.id.camera)
+
+        registerForContextMenu(imageButton)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_context_photo, menu)
     }
 }

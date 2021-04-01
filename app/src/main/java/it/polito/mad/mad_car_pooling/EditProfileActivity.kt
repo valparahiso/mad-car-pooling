73d1cd2a61ff63e8e319e8f87c2865301c2f3489
Enrico <bravienrico@yahoo.it>
2021-04-01 10:44:53 +0200

    fixed EditText width

--- app/src/main/java/it/polito/mad/mad_car_pooling/EditProfileActivity.kt
+++ app/src/main/java/it/polito/mad/mad_car_pooling/EditProfileActivity.kt
@@ -2,9 +2,12 @@ package it.polito.mad.mad_car_pooling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ContextMenu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ImageButton
 
 class EditProfileActivity : AppCompatActivity() {
@@ -21,5 +24,20 @@ class EditProfileActivity : AppCompatActivity() {
         super.onCreateContextMenu(menu, v, menuInfo)
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu_context_photo, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        //val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+        return when (item.itemId) {
+            R.id.gallery -> {
+                Log.d("POLITOMAD","Gallery")
+                true
+            }
+            R.id.camera -> {
+                Log.d("POLITOMAD","Photo")
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
     }
 }

bc357ef1528a3a6482b30eab4a2cf1c2b4a3fdb6
Enrico <bravienrico@yahoo.it>
2021-04-03 10:14:04 +0200

    fixed save state on rotate

--- app/src/main/java/it/polito/mad/mad_car_pooling/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/mad_car_pooling/ShowProfileActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -14,20 +15,36 @@ class ShowProfileActivity : AppCompatActivity() {
     private lateinit var fullName: TextView
     private lateinit var nickName: TextView
     private lateinit var location: TextView
-    private lateinit var emain: TextView
+    private lateinit var email: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
         fullName = findViewById<TextView>(R.id.fullName)
         nickName = findViewById(R.id.nickname)
-        emain = findViewById(R.id.email)
+        email = findViewById(R.id.email)
         location = findViewById(R.id.location)
 
         fullName.text =  "Mario Rossi"
         nickName.text =  "mariored89"
-        emain.text =  "mario.rossi@polito.it"
+        email.text =  "mario.rossi@polito.it"
         location.text =  "Lombardia"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("fullname", fullName.text.toString())
+        outState.putString("nickname", nickName.text.toString())
+        outState.putString("email", email.text.toString())
+        outState.putString("location", location.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        fullName.text = savedInstanceState.getString("fullname")
+        nickName.text = savedInstanceState.getString("nickname")
+        email.text = savedInstanceState.getString("email")
+        location.text = savedInstanceState.getString("location")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -46,7 +63,7 @@ class ShowProfileActivity : AppCompatActivity() {
         val intent = Intent(this, EditProfileActivity::class.java)
         intent.putExtra("group02.lab1.FULL_NAME", fullName.text)
         intent.putExtra("group02.lab1.NICK_NAME", nickName.text)
-        intent.putExtra("group02.lab1.EMAIL", emain.text)
+        intent.putExtra("group02.lab1.EMAIL", email.text)
         intent.putExtra("group02.lab1.LOCATION", location.text)
         startActivityForResult(intent, 1)
     }
@@ -62,7 +79,7 @@ class ShowProfileActivity : AppCompatActivity() {
     private fun showProfile(data: Intent?) {
         fullName.text = data?.getStringExtra("group02.lab1.FULL_NAME")
         nickName.text = data?.getStringExtra("group02.lab1.NICK_NAME")
-        emain.text = data?.getStringExtra("group02.lab1.EMAIL")
+        email.text = data?.getStringExtra("group02.lab1.EMAIL")
         location.text = data?.getStringExtra("group02.lab1.LOCATION")
     }
 }

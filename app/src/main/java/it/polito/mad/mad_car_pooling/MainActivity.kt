a4255eb4c6ea5417eca2f9d198d7956460803933
Enrico <bravienrico@yahoo.it>
2021-04-19 10:04:39 +0200

    added fragments

--- app/src/main/java/it/polito/mad/mad_car_pooling/MainActivity.kt
+++ app/src/main/java/it/polito/mad/mad_car_pooling/MainActivity.kt
@@ -1,24 +1,68 @@
 package it.polito.mad.mad_car_pooling
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import androidx.navigation.ui.NavigationUI
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.commit
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.tripListReciclerView)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
-        val list: List<String> = listOf("Stringa1", "Stringa2", "Stringa3", "Stringa1", "Stringa2", "Stringa3","Stringa1", "Stringa2", "Stringa3" , "Stringa1", "Stringa2", "Stringa3")
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = TripAdapter(list)
+        val fragment = supportFragmentManager.findFragmentByTag("tripListFragmentTag")
+        if (fragment == null) supportFragmentManager.commit {
+            add(R.id.fragment_container_view, TripListFragment(), "tripListFragmentTag")
+            //addToBackStack("secondIn")
+        }
+        /*else {
+            supportFragmentManager.commit {
+                remove(fragment)
+                //addToBackStack("secondOut")
+            }
+         */
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 }
